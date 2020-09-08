0bcd9d515bae3f9c2beffb9d3b03bffc23ff6967
Michael-Xie <xiemicha@gmail.com>
2020-09-08 04:43:39 -0400

    Updates

---
+++ precache-manifest.c3f940a776adf1e3a56007336f36dc2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cbb6621f1d2c19b30b44aeb41cab2a",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "8d3334c5c888e3378a24",
+    "url": "/shoppies/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d691e9f8a4b61b42ab54",
+    "url": "/shoppies/static/js/2.3967312b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/shoppies/static/js/2.3967312b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3334c5c888e3378a24",
+    "url": "/shoppies/static/js/main.7e3db670.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  }
+]);
